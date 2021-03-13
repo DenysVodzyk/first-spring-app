8aac25cb4d702a1337d99a8c8261d3b3541956b0
DenysVodzyk <vodzyk@gmail.com>
2021-03-12 23:45:22 -0500

    fixed main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.HelloWorldService;
 import service.IGreetingService;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-        IGreetingService service = (IGreetingService) ctx.getBean("HelloWorldService");
+        IGreetingService service = (HelloWorldService) ctx.getBean("helloWorldService");
         service.sendGreeting();
     }
 }
