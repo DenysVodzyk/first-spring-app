83923963da984b4d288f4de3cc4f2f6d385a4ab8
DenysVodzyk <vodzyk@gmail.com>
2021-03-13 21:47:02 -0500

    added constructor

--- src/main/java/service/HelloWorldService.java
+++ src/main/java/service/HelloWorldService.java
@@ -1,7 +1,17 @@
 package service;
 
 public class HelloWorldService implements IGreetingService {
+    public String message;
+
+    public HelloWorldService() {
+        this.message = "";
+    }
+
+    public HelloWorldService(String message) {
+        this.message = message;
+    }
+
     public void sendGreeting() {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world!" + message);
     }
 }
