27a632f39b59d59cfa4ea8d0ee34f9795f45e527
DenysVodzyk <vodzyk@gmail.com>
2021-03-13 21:48:11 -0500

    minor changes

--- src/main/java/service/HelloKittyService.java
+++ src/main/java/service/HelloKittyService.java
@@ -1,6 +1,8 @@
 package service;
 
 public class HelloKittyService implements IGreetingService {
+    private HelloWorldService helloWorldService;
+
     public void sendGreeting() {
         System.out.println("Hello, Kitty!");
     }
